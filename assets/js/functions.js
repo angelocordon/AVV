a57ca74b5a9abe660d0965fa72e12931831518f7
Angelo Cordon <angelocordon@gmail.com>
2015-07-20 13:49:45 -0700

    Optimized footer links, fixed up css

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,3 +3,21 @@ $( document ).ready(function() {
   //
 
 });
+
+
+
+var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
+
+  document.querySelector('.menu').addEventListener('click', function(eve) {
+  if (eve.target.nodeName === 'A') { slideout.close(); }
+  });
