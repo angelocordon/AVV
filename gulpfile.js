8fe85bd8a0a821b19fb2fe6de1c4de208aa532f4
Angelo Cordon <angelocordon@gmail.com>
2016-02-19 11:13:26 -0800

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('sass', function(){
+  return gulp.src('app/assets/css/abigail.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/assets/css/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/assets/js/*.js', browserSync.reload);
+});
+
+gulp.task('build', function(callback){
+  runSequence('clean:dist', ['sass', 'useref'], callback
+  )
+});
+
+gulp.task('default', function(callback){
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  )
+});
